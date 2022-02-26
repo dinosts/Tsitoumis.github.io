75b1b455d461a3ce7e0f2ffea6b7961ce397cc4f
Konstantinos Tsitoumis <49339421+dinosts@users.noreply.github.com>
2022-02-26 14:53:41 +0200

    welcome bio

--- js/cards/welcome.js
+++ js/cards/welcome.js
@@ -14,7 +14,7 @@ const underTitle = "<p id='text' class=''> Software Engineer </p>";
 const { connections } = constants;
 
 const bio =
-  "<p id='bio' class='py-3'>Coding with greek meraki and love.<br/><br/>Currently obsessed with Node.js.<br/><br/>Some of his main goals the improvement of his Software engineering skills, being active and mostly have fun in and outside of his career life.<br/><br/>Currently in love with Node.js.<br/><br/>Mainly enjoying working at the backend, but a fine engineer can work on anything.<br/><br/>Some of his main goals the improvement of his Software engineering skills, being active and mostly have fun in and outside of his career life. </p>";
+  "<p id='bio' class='py-3'>Coding with greek meraki and love.<br/><br/>Currently obsessed with Node.js.<br/><br/>Mainly enjoying working at the backend, but a fine engineer can work on anything.<br/><br/>Some of his main goals the improvement of his Software engineering skills, being active and mostly have fun in and outside of his career life. </p>";
 
 const welcomeCard = `${image}${name}${underTitle}${bio}${connections}`;
 
